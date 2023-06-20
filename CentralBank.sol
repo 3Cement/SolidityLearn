f2931478b07d893eba1e9a403e06b5d4051e3fbf
Daniel <30894056+3Cement@users.noreply.github.com>
2023-06-20 08:44:52 +0200

    Create Payable function

--- CentralBank.sol
+++ CentralBank.sol
@@ -4,23 +4,16 @@ pragma solidity 0.8.18;
 
 contract CentralBank {
     address private owner;
-    uint256 public totalBalance = 1000;
+    uint256 public totalBalance;
     mapping(address => uint) public balances;
 
-    constructor (uint256 _totalBalance) {
+    constructor () {
         owner = msg.sender;
-        totalBalance = _totalBalance;
     }
 
-    function increaseTotalBalance(uint amount) public {
-        totalBalance = totalBalance + amount;
-    }
-
-    function createAccount(address newAcc, uint amount) public {
-        require(msg.sender == owner, "sender is unauthorized");
-        require(amount <= totalBalance, "amount is incorrect");
-
-        balances[newAcc] = amount;
-        totalBalance = totalBalance - amount;
+    function deposit() public payable {
+        require(msg.value > 0, "deposit more then zero");
+        totalBalance += msg.value;
+        balances[msg.sender] += msg.value;
     }
 }
