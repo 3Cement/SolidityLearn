74004352fa7c5d03b52ec352140bb26536e2790a
Daniel <30894056+3Cement@users.noreply.github.com>
2023-06-20 09:10:49 +0200

    Create whitelist and checked if user is on the whitelist.

--- CentralBank.sol
+++ CentralBank.sol
@@ -6,18 +6,26 @@ contract CentralBank {
     address private owner;
     uint256 public totalBalance;
     mapping(address => uint) public balances;
+    mapping(address => bool) public whitelistedAddrs;
 
     constructor () {
         owner = msg.sender;
     }
 
+    function whitelist(address payable addr, bool flag) public {
+        require(msg.sender == owner, "caller is not an owner");
+        whitelistedAddrs[addr] = flag;
+    }
+
     function deposit() public payable {
+        require(whitelistedAddrs[msg.sender] == true, "caller is not whitelisted");
         require(msg.value > 0, "deposit more then zero");
         totalBalance += msg.value;
         balances[msg.sender] += msg.value;
     }
 
     function withdraw(uint amount) public {
+        require(whitelistedAddrs[msg.sender] == true, "caller is not whitelisted");
         require(balances[msg.sender] >= amount, "insufficient funds");
 
         (bool sent,) = msg.sender.call{value: amount}("");
