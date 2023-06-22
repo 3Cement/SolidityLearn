158c6d29119d16e9495f9bd15fe353438ce8e0ba
Daniel <30894056+3Cement@users.noreply.github.com>
2023-06-22 09:37:16 +0200

    Call external existing contract using IWhitelister

--- CentralBank.sol
+++ CentralBank.sol
@@ -4,31 +4,32 @@ pragma solidity 0.8.19;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
+contract IWhitelister {
+    function istWhitelisted(address _wallet) public view returns(bool) {}
+} 
+
 contract CentralBank is Ownable(msg.sender) {
     uint256 public totalBalance;
 
     mapping(address => uint256) private balances;
-    mapping(address => bool) public whitelistedAddrs;
     mapping(address => uint256) public lastDepositAt;
 
     event NewDepostit(address indexed wallet, uint256 amount);
 
     modifier whitelistedOnly() {
-        require(whitelistedAddrs[msg.sender] == true, "caller is not whitelisted");
+        require(
+            IWhitelister(0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47).istWhitelisted(msg.sender) == true, 
+            "caller is not whitelisted");
         _;
     }
 
     function balanceOf(address addr) public view returns(uint256) {
         return balances[addr];
     }
-
-    function whitelist(address payable addr, bool flag) public onlyOwner {
-        whitelistedAddrs[addr] = flag;
-    }
-
+    
     function deposit() public payable whitelistedOnly {
         require(msg.value > 0, "deposit more then zero");
-        
+
         totalBalance += msg.value;
         balances[msg.sender] += msg.value;
         emit NewDepostit(msg.sender, msg.value);
