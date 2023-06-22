f8bef60dfbaa6137880541ef3440de9aec41bb34
Daniel <30894056+3Cement@users.noreply.github.com>
2023-06-22 09:53:19 +0200

    Store external address on immutable variable called whitelistRegistry and format code.

--- CentralBank.sol
+++ CentralBank.sol
@@ -5,25 +5,32 @@ pragma solidity 0.8.19;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 interface IWhitelister {
-    function istWhitelisted(address _wallet) external view returns(bool);
-} 
+    function istWhitelisted(address _wallet) external view returns (bool);
+}
 
 contract CentralBank is Ownable(msg.sender) {
     uint256 public totalBalance;
+
+    address immutable whitelistRegistry;
 
     mapping(address => uint256) private balances;
     mapping(address => uint256) public lastDepositAt;
 
     event NewDepostit(address indexed wallet, uint256 amount);
 
+    constructor(address whitelistRegistryAddr) {
+        whitelistRegistry = whitelistRegistryAddr;
+    }
+
     modifier whitelistedOnly() {
         require(
-            IWhitelister(0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47).istWhitelisted(msg.sender) == true, 
-            "caller is not whitelisted");
+            IWhitelister(whitelistRegistry).istWhitelisted(msg.sender) == true,
+            "caller is not whitelisted"
+        );
         _;
     }
 
-    function balanceOf(address addr) public view returns(uint256) {
+    function balanceOf(address addr) public view returns (uint256) {
         return balances[addr];
     }
 
@@ -33,14 +40,17 @@ contract CentralBank is Ownable(msg.sender) {
         totalBalance += msg.value;
         balances[msg.sender] += msg.value;
         emit NewDepostit(msg.sender, msg.value);
-        lastDepositAt[msg.sender] = block.timestamp ;
+        lastDepositAt[msg.sender] = block.timestamp;
     }
 
     function withdraw(uint256 amount) public whitelistedOnly {
         require(balances[msg.sender] >= amount, "insufficient funds");
-        require(block.timestamp > lastDepositAt[msg.sender] + 2 minutes, "withdraw not yet possible");
+        require(
+            block.timestamp > lastDepositAt[msg.sender] + 2 minutes,
+            "withdraw not yet possible"
+        );
 
-        (bool sent,) = msg.sender.call{value: amount}("");
+        (bool sent, ) = msg.sender.call{value: amount}("");
         require(sent, "Failed to send Ether");
         totalBalance -= amount;
         balances[msg.sender] -= amount;
